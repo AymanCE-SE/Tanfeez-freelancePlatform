d4a7739f5bb66215a157dc5a41042ad17d4613ff
ayman samir <ayman_Samir_ahmed@hotmail.com>
2026-08-30 00:23:48 +0300

    ordering asgi import

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,11 +1,12 @@
 import os
 from django.core.asgi import get_asgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chatroom.routing import websocket_urlpatterns as chat_ws
 from notification.routing import websocket_urlpatterns as notification_ws
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
