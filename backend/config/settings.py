49ab4e6e309e0086616369d64b14cc3fa5d12ee3
ayman samir <ayman_Samir_ahmed@hotmail.com>
2026-09-16 03:47:32 +0300

    Fix CORS middleware order and settings and add testing lines

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     "channels",
     "cloudinary_storage",
     ###
-    "rest_framework",
     "corsheaders",
+    "rest_framework",
     "user",
     "freelancer",
     "client",
@@ -63,10 +63,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware", 
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -223,3 +223,8 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+#Testing linesprint("========== DJANGO SETTINGS BOOT LOG ==========")
+print("1. CORS_ALLOW_ALL_ORIGINS is set to:", CORS_ALLOW_ALL_ORIGINS)
+print("2. The allowed frontend URL is:", CORS_ALLOWED_ORIGINS)
+print("==============================================")
