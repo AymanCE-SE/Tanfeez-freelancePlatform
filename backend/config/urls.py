8c92467ea1420887be55df7c9640030475aa2a6d
ayman samir <ayman_Samir_ahmed@hotmail.com>
2026-09-16 06:14:48 +0300

    Add health url for back4app server

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,8 +19,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK - Tanfeez Backend is Alive", status=200)
 
 urlpatterns = [
+    path('', health_check, name='health_check'),
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls")),
     path("api/freelancers/", include("freelancer.urls")),
